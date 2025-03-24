21cbefddaa747be788bbb695d4d6b9a50007a8f3
Hadayetullah <hadayetullah30@gmail.com>
2025-03-24 20:46:45 +0600

    Update to load media and static files

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,10 @@
-
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('app_useraccount.urls')),
     path('api/customer/', include('app_customer.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
